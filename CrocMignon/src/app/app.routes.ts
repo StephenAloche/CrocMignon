f3772903ef342294ce246da13e4735d8a111c36d
StephenAloche <stephen.aloche@gmail.com>
2025-02-20 00:40:40 +0100

    miseEnPlace: [] premier squelette

--- CrocMignon/src/app/app.routes.ts
+++ CrocMignon/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from '../Contact/contact/contact.component';
+import { PresentationComponent } from '../Presentation/presentation/presentation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'presentation', loadComponent: () => import('../Presentation/presentation/presentation.component').then(m => m.PresentationComponent) },
+    { path: 'contact', loadComponent: () => import('../Contact/contact/contact.component').then(m => m.ContactComponent) },
+    { path: '**', redirectTo: '' }
+];
