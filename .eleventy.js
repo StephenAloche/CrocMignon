6ce63ccccf46b8c06faa9176eadc0f625478b95f
StephenAloche <stephen.aloche@gmail.com>
2025-02-23 00:02:03 +0100

    correction path

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
 module.exports = function(eleventyConfig) {
     // Copy `src/css/` to `_site/css/`
     eleventyConfig.addPassthroughCopy("src/css");
+
+  let isProduction = process.env.ELEVENTY_ENV === "production";
+  let pathPrefix = isProduction ? "/CrocMignon/" : "/";
+
+  // Check if a custom domain is set (GitHub Pages uses CNAME for this)
+  if (process.env.CUSTOM_DOMAIN) {
+    pathPrefix = "/"; // No subfolder needed with a custom domain
+  }
   
     return {
+      pathPrefix: pathPrefix,
       dir: {
         input: "src",
         output: "_site",
